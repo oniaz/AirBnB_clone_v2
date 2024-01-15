0b6be97289789330d2ea810fbd9c9fbccdcfdaed
oniaz <32391049+oniaz@users.noreply.github.com>
2024-01-15 04:35:46 +0200

    user model tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -18,3 +18,19 @@ class TestUser(unittest.TestCase):
         """ new isntance, ID attribute exists"""
         u = User()
         self.assertTrue(hasattr(u, 'id'))
+
+    def test_user_email(self):
+        """tests the email class attribute"""
+        self.assertEqual("", User.email)
+
+    def test_user_password(self):
+        """tests the password class attribute"""
+        self.assertEqual("", User.password)
+
+    def test_user_first_name(self):
+        """tests the first_name class attribute"""
+        self.assertEqual("", User.first_name)
+
+    def test_user_last_name(self):
+        """tests the last_name class attribute"""
+        self.assertEqual("", User.last_name)
